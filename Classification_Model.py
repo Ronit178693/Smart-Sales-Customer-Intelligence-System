5c92577f02eaebf4edaca8ee9b5517580e938fc9
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-07 11:23:23 +0530

    Tuned Hyperparameters using RandomSearchCV

--- Classification_Model.py
+++ Classification_Model.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import loguniform
 # Importing processed data directly
 from Data_Preprocessing import train_x, test_x, train_y, test_y
 
@@ -21,10 +23,36 @@ print("Cross-Validation Scores are: ", cv_scores)
 print("Mean Accuracy:", cv_scores.mean())
 #  Our model performs well
 
-LR.fit(train_x, train_y['Churn_Label'])
+#Performing Randomized Search CV
+#These are the hyperparameters we are going to tune
+param_dist = {
+    "C": loguniform(1e-4, 1e2),
+    "penalty": ["l1", "l2"],
+    "solver": ["liblinear"],
+    "class_weight": [None, "balanced"]
+}
+
+random_search = RandomizedSearchCV(
+    estimator=LR,
+    param_distributions=param_dist,
+    n_iter=30,
+    cv=5,
+    scoring="accuracy",
+    n_jobs=-1,
+    random_state=42
+)
+
+random_search.fit(train_x, train_y['Churn_Label'])
+
+print("Best Parameters:", random_search.best_params_)
+print("Best Accuracy:", random_search.best_score_)
+
+# Using the best model from RandomizedSearch
+best_model = random_search.best_estimator_
+best_model.fit(train_x, train_y['Churn_Label'])
 
 # Predictions and Evaluation
-pred_y = LR.predict(test_x)
+pred_y = best_model.predict(test_x)
 print("Classification Report:")
 print(classification_report(test_y['Churn_Label'], pred_y))
 
