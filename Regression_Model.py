c1114377d59cf299555c850c654ef77bd5cf31f7
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-07 17:04:50 +0530

    Performed Cross-Validation on Regression Model

--- Regression_Model.py
+++ Regression_Model.py
@@ -3,12 +3,25 @@ import pandas as pd
 from Data_Preprocessing import train_x, test_x, train_y, test_y
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score
 
 
 df = pd.read_csv("preprocessed_data.csv")
 
 
 Linear = LinearRegression()
+cross_val = cross_val_score(
+    Linear,
+    train_x,
+    train_y['Next_Month_Spend_Label'],
+    cv=5,
+    scoring="r2"
+)
+
+print("Cross Validation Scores: ", cross_val)
+print("Mean Cross Validation Score: ", np.mean(cross_val))
+# Model is performing good while Cross Validating
+
 Linear.fit(train_x, train_y['Next_Month_Spend_Label'])
 
 pred_y = Linear.predict(test_x)
