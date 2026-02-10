779ec9bb24367fbaa4eb8bae712376586e5075da
Ronit178693 <ronitagrawal9921@gmail.com>
2026-02-10 14:51:11 +0530

    Using KMeans for Clustering

--- Unsupervised_model.py
+++ Unsupervised_model.py
@@ -12,3 +12,29 @@ for i in range(2, 11):
     kmeans = KMeans(n_clusters = i, random_state = 42)
     kmeans.fit(train_x)
     wcss.append(kmeans.inertia_)
+
+plt.figure(figsize = (10,7))
+sns.lineplot(x = range(2, 11), y = wcss, marker = 'o')
+plt.title('Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# The optimal K will be either 4, 5
+
+#Initializing the K-Means Algorithm
+Kmeans = KMeans(
+    n_clusters = 4,
+    random_state = 42,
+    init = 'k-means++', # Using k-means++ for better initialization
+    n_init = 10 # Number of times the algorithm will run with different centroid seeds
+)
+#Model Training on the training dataset
+Kmeans.fit(train_x)
+#Predicting the clusters
+Kmeans.predict(train_x)
+Kmeans.predict(test_x)
+#Getting the Centroids
+centroid = Kmeans.cluster_centers_
+print("Centroids: ", centroid)
+
